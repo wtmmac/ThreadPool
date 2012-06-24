6eaf04d9fc079f094f2ed8ffb66e3015340a2928
YummyBian <yummy.bian@gmail.com>
2012-06-24 23:27:52 +0800

    Update

--- threadpool.py
+++ threadpool.py
@@ -1,9 +1,9 @@
 import threading, Queue, time, sys
 
 
-class Worker(Threading.Thread):
+class Worker(threading.Thread):
     def __init__(self, inQueue, outQueue, errQueue):
-        Threading.Thread._ _init_ _(self, **kwds)
+        threading.Thread.__init__(self)
         self.setDaemon(True)
         self.inQueue = inQueue
         self.outQueue = outQueue
@@ -13,7 +13,7 @@ class Worker(Threading.Thread):
     
     def run(self):
         while True:
-            if !self.active:
+            if not self.active:
                 break
             try:
                 callable, args, kwds = self.inQueue.get()
@@ -37,13 +37,13 @@ class ThreadPool():
         self.errQueue = Queue.Queue(poolSize)
         self.pool = {}
         for i in range(numThreads):
-            newThread = Worder(self.inQueue, self.outQueue, self.errQueue)
+            newThread = Worker(self.inQueue, self.outQueue, self.errQueue)
             self.pool[i] = newThread
             
     def addTask(self, callable, *args, **kwds):
         self.inQueue.put((callable, args, kwds))
 
-    def getAllResults(self, queue):
+    def _getAllResults(self, queue):
         ''' generator to yield one after the others all items currently
             in the queue, without any waiting
         '''
@@ -57,19 +57,37 @@ class ThreadPool():
         return self.outQueue.get()     # implicitly stops and waits
 
     def showAllResults(self):
-        for result in getAllResults(self.outQueue):
+        for result in self._getAllResults(self.outQueue):
             print 'Result:', result
         
     def showAllErrors(self):
-        for etyp, err in get_all_from_queue(self.errQueue):
+        for etyp, err in self._getAllResults(self.errQueue):
             print 'Error:', etyp, err
 
     def dismissWorkers(self):
         # order is important: first, request all threads to stop...:
-        for i in pool:
-            self.pool[i].dismiss
+        for i in self.pool:
+            self.pool[i].dismiss()
         # ...then, wait for each of them to terminate:
-        for i in Pool:
+        for i in self.pool:
             self.pool[i].join()
         # clean up the pool from now-unused thread objects
-        del Pool
+        del pool
+
+
+cnt = 0
+
+def callback():
+   global cnt
+   cnt += 1
+   print "hello"
+   return True 
+
+if __name__ == "__main__":
+   pool = ThreadPool(5)
+   pool.addTask(callback())
+   pool.showAllResults()
+   pool.dismissWorkers()
+
+
+
