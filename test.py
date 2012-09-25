573092b1973830b8798fba8d211de4361f900747
yummybian <yummy.bian@gmail.com>
2012-09-25 10:57:17 +0800

    Update test.py

--- test.py
+++ test.py
@@ -18,7 +18,5 @@ if __name__ == "__main__":
     logging.debug('Add 10 tasks to thread pool.')
     for i in range(20):
         pool.add_task(doWork, i, a=i)
-    logging.debug('Wait for a while untill all tasks have been processed')
-    time.sleep(1)
     logging.debug('Destroy thread pool and join threads')
     pool.destroy()
