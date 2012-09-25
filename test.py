3b3bff39ba52e7cab117025e1a47ba6da9b85dab
yummybian <yummy.bian@gmail.com>
2012-09-25 10:40:55 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,24 @@
-import time
 import threadpool
+import logging
+import time
+
+logging.basicConfig(
+        level=logging.DEBUG,
+        format='[%(levelname)s] (%(threadName)-10s) %(message)s',
+        )
 
 def doWork(*args, **kwds):
-    return kwds 
+    logging.debug('starting')
+    logging.debug('args is ' + str(args) + ' kwds is ' + str(kwds))
+    logging.debug('exiting')
 
 if __name__ == "__main__":
-    pool = threadpool.ThreadPool(5)
-    for i in range(10):
-        pool.addTask(doWork, a=i, b=i*2)
-    # wait for a while untill all tasks have been processed
+    logging.debug('Initialize thread pool and spawn 3 threads.')
+    pool = threadpool.ThreadPool(3)
+    logging.debug('Add 10 tasks to thread pool.')
+    for i in range(20):
+        pool.add_task(doWork, i, a=i)
+    logging.debug('Wait for a while untill all tasks have been processed')
     time.sleep(1)
-    pool.showAllResults()
-    pool.dismissWorkers()
+    logging.debug('Destroy thread pool and join threads')
+    pool.destroy()
