75149bc3d0fa999f6324f60455c7087bfc349715
yummybian <yummy.bian@gmail.com>
2012-10-08 22:22:54 +0800

    Update threadpool.py

--- threadpool.py
+++ threadpool.py
@@ -2,7 +2,7 @@
 #
 # ThreadPool is a simple thread pool
 #
-# Copyright (C) Yummy.Bian 2012 <yummy.bian#gmail.com>
+# Copyright (C) Yummy Bian 2012 <yummy.bian#gmail.com>
 #
 # under the terms of the GNU Lesser General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
